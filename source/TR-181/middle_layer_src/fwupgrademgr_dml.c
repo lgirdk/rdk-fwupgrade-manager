2cd6fd17707a734f22aad3bbb5d8a0c043adda13
mkamat <mkamat@libertyglobal.com>
2023-02-08 15:19:18 -0800

    OFW-4118 [Mv3] CPE is not rejecting the second Download RPC

--- source/TR-181/middle_layer_src/fwupgrademgr_dml.c
+++ source/TR-181/middle_layer_src/fwupgrademgr_dml.c
@@ -372,6 +372,7 @@ FirmwareUpgrade_SetParamBoolValue
     )
 {
     PDEVICE_INFO pMyObject = (PDEVICE_INFO) g_pBEManager->pDeviceInfo;
+    BOOL  retValue = FALSE;
     /* check the parameter name and set the corresponding value */
     if (strcmp(ParamName, "X_RDKCENTRAL-COM_FirmwareDownloadNow") == 0)
     {
@@ -379,16 +380,22 @@ FirmwareUpgrade_SetParamBoolValue
         {
             if(bValue)
             {
-                FwDlDmlDIDownloadNow((ANSC_HANDLE)pMyObject);
+               if(FwDlDmlDIDownloadNow((ANSC_HANDLE)pMyObject) == ANSC_STATUS_SUCCESS)
+               {
+                   retValue = TRUE;
+               }
+               else
+               {
+                   CcspTraceError(("FwDlDmlDIDownloadNow failed\n"));
+               }
             }
         }
         else
         {
             CcspTraceError(("FW DL is not allowed for this image type \n"));
         }
-        return 1;
     }
-    return 0;
+    return retValue;
 }
 
 /**********************************************************************
