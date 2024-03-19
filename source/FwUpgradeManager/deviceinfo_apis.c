baa91d728f701c14aed408c1c11777aa4f49d335
Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>
2024-03-19 01:03:49 -0400

    fix Graceful/Soft reset not possible on Hub5x ( OFW-5644 F5685SI-2041 )
    [Bug Description]
    Delay the reboot while Line 1 or Line 2 are in active status
    
    [Fix Description]
    Added the logic to delay the reboot by checking the status of Voice Lines (Line 1 or Line2 )
    Used the voice status datamodel (Device.Services.VoiceService.1.CallControl.Line.{i}.CallStatus for i , 1 & 2)
    to identify the status of Voice calls
    Idle/Disconnected : line is not in a call  - Proceed for reboot
    Connected : line is in a call              - Delay the reboot
    Dialing/Delivered/Alerting : line call extablishement in progress (not yet in a call)  - Delay the reboot
    
    [Testing Performed]
    1. Trigger Download RPC tests to cover all the positive and negative cases
    2. Performed functional testing with different voice call status(Idle,Disconnected,Connected,Dialing,Delivered,Alerting)
    
    Upstream Candidate: No. Code changes are specific to LGI
    
    Signed-off-by: Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>

--- source/FwUpgradeManager/deviceinfo_apis.c
+++ source/FwUpgradeManager/deviceinfo_apis.c
@@ -40,6 +40,7 @@
 #include "cap.h"
 #include <syscfg/syscfg.h>
 #include <ccsp_syslog.h>
+#include "ccsp_psm_helper.h"
 #ifdef FEATURE_RDKB_LED_MANAGER
 #include <sysevent/sysevent.h>
 extern int sysevent_fd ;
@@ -352,6 +353,11 @@ void FwDl_ThreadFunc()
     app_caps.caps = NULL;
     app_caps.user_name = NULL;
     char sysbuf[16];
+    char *paramNames[]= { "Device.Services.VoiceService.1.CallControl.Line.1.CallStatus", "Device.Services.VoiceService.1.CallControl.Line.2.CallStatus" };
+    parameterValStruct_t **valStrCallStatus;
+    char *compo = "eRT.com.cisco.spvtg.ccsp.telcovoicemanager";
+    char *bus = "/com/cisco/spvtg/ccsp/telcovoicemanager";
+    int  nval = 0;
 
     pthread_detach(pthread_self());
     CcspTraceInfo(("Gaining root permission to download and write the code to flash \n"));
@@ -490,6 +496,51 @@ void FwDl_ThreadFunc()
 
     if (dl_status == 200)
     {
+        for (int i = 0; i <= 1440; i++) /* Check whether it reached 24hrs (24 * 60) and if so, proceed to reboot irrespective of voice status */
+        {
+            ret = CcspBaseIf_getParameterValues(bus_handle,
+                                                compo,
+                                                bus,
+                                                paramNames,
+                                                2,
+                                                &nval,
+                                                &valStrCallStatus);
+            if ((CCSP_SUCCESS == ret) && (nval == 2))
+            {
+                if (strcmp(valStrCallStatus[0]->parameterValue, "Connected") == 0 ||
+                    strcmp(valStrCallStatus[0]->parameterValue, "Dialing") == 0 ||
+                    strcmp(valStrCallStatus[0]->parameterValue, "Delivered") == 0 ||
+                    strcmp(valStrCallStatus[0]->parameterValue, "Alerting") == 0 ||
+                    strcmp(valStrCallStatus[1]->parameterValue, "Connected") == 0 ||
+                    strcmp(valStrCallStatus[1]->parameterValue, "Dialing") == 0 ||
+                    strcmp(valStrCallStatus[1]->parameterValue, "Delivered") == 0 ||
+                    strcmp(valStrCallStatus[1]->parameterValue, "Alerting") == 0)
+                {
+                    CcspTraceError(("Reboot is delayed due to active call or call establishment in progress.\n"));
+                    sleep(60);
+                }
+                else if ((strcmp(valStrCallStatus[0]->parameterValue, "Idle") == 0 ||
+                          strcmp(valStrCallStatus[0]->parameterValue, "Disconnected") == 0) &&
+                         (strcmp(valStrCallStatus[1]->parameterValue, "Idle") == 0 ||
+                          strcmp(valStrCallStatus[1]->parameterValue, "Disconnected") == 0))
+                {
+                    CcspTraceInfo(("Line is not in a call.\n"));
+                    break;
+                }
+                else
+                {
+                    CcspTraceError(("Unknown call status.\n"));
+                }
+            }
+            else
+            {
+                CcspTraceError(("Failed to get call status. Exiting loop.\n"));
+                break;
+            }
+
+            CcspTraceInfo(("CallStatus of Line 1 & Line 2 are %s & %s\n", valStrCallStatus[0]->parameterValue, valStrCallStatus[1]->parameterValue));
+        }
+
         ret = fwupgrade_hal_download_reboot_now();
 
         if(ret == ANSC_STATUS_SUCCESS)
